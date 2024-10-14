fdafbc37fc47a978f21d3006d63b266f806ffc8d
Avigail Shklovsky <9013825@gmail.com>
2024-10-14 09:49:34 +0300

    initial table

--- library.js
+++ library.js
@@ -1,25 +1,50 @@
 import books from './books.json' with { type: 'json' };
 console.log(books);
-
+const booksData=books.books;
 // books.map(book=>{
 //     console.log(book.title);
     
 // })
 
 function func(){
-    let list=document.getElementById('bookList');
-    list.style.backgroundColor='red'
-    let ul=document.createElement('ul');
-    list.appendChild(ul)
-    for (let i = 0; i < books.length; i++) {
-        const book = books[i];
-        let newBook=document.createElement('li')
-        console.log(newBook);
-        
-        newBook.innerHTML=`${book.title}`;
-        list.appendChild(newBook);
-    }
+
+    // {
+    //     "id": 1,
+    //     "title": "To Kill a Mockingbird",
+    //     "author": "Harper Lee",
+    //     "genre": "Fiction",
+    //     "published_year": 1960,
+    //     "description": "A novel about the serious issues of rape and racial inequality."
+    //   }
     
+    const tableBody = document.querySelector('#bookTable');   
+    
+    booksData.forEach(item => {
+        const newBook = document.createElement('tr');
+        
+        const idCell = document.createElement('td');
+        idCell.textContent = item.id;
+        newBook.appendChild(idCell);
+
+        const titleCell= document.createElement('td');
+        titleCell.textContent=item.title;
+        newBook.appendChild(titleCell);
+        
+        const priceCell= document.createElement('td');
+        priceCell.textContent=`${item.price}$`;
+        newBook.appendChild(priceCell);
+
+        const openCell = document.createElement('td');
+        openCell.textContent = 'Open'
+        openCell.id='openBook';
+        openCell.addEventListener('click', () => openBook(item));
+        newBook.appendChild(openCell);
+
+        console.log(newBook);
+        tableBody.appendChild(newBook);
+    });
+
+
 }
 
     window.onload=func;
